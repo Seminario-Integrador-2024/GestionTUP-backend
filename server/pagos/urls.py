93837891ba464ce0893177f32f016d31aa9f0cb7
ulisespallares888 <ulisespallares8@gmail.com>
2024-09-09 18:07:25 -0300

    Areglos finales en  comdepag

--- server/pagos/urls.py
+++ server/pagos/urls.py
@@ -16,7 +16,7 @@ router.register(r"cuotas", CuotaViewSet)
 app_name = 'pagos'
 urlpatterns = [
     path("", include(router.urls)),
-    path('compromisos/archivo/<int:pk>/', CompromisoDePagoViewSet.as_view({'get': 'retrieve-pdf'}), name='retrieve-pdf'),
+    path('compromisos/archivo/<int:pk>/', CompromisoDePagoViewSet.as_view({'get': 'retrieve_pdf'}), name='retrieve_pdf'),
     path('ultimo-compr-de-pag/', UltimoCompromisoDePago.as_view(), name='ultimo-compromiso-de-pago'),
     path('firmas-de-alumno/<int:alumno_id>/', FirmasDeUnAlumnoView.as_view({'get': 'list'}), name='firmas-de-alumno'),
     path('firmar-compromiso/<int:alumno_id>/', FirmarCompromisoView.as_view(), name='firmar-compromiso'),
