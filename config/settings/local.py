d3c5986d032c09472341427230fa4fd770f88ff7
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2024-09-13 20:26:24 -0300

    Add CORS_ALLOW_ALL_ORIGINS setting to local configuration

--- config/settings/local.py
+++ config/settings/local.py
@@ -15,7 +15,7 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
-
+CORS_ALLOW_ALL_ORIGINS = True
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
