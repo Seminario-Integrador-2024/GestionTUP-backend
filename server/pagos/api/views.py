1bc7504abb4a45fe3b033c8b88f6431d36fd158f
Tobias Maciel <tobiasmaciel03@gmail.com>
2024-09-30 20:05:58 -0300

    feat: Nuevas views

--- server/pagos/api/views.py
+++ server/pagos/api/views.py
@@ -108,12 +108,52 @@ class CompromisoDePagoViewSet(viewsets.ModelViewSet):
 
 #Esta vista no es usada
 class FirmaCompPagoAlumnoViewSets(viewsets.ModelViewSet):
-    queryset = FirmaCompPagoAlumno.objects.all()
-    serializer_class = FirmaCompPagoAlumnoSerializer
     pagination_class = FirmasResultSetPagination
 
 
-            
+class AlumnosFirmaronUltimoCompromisoView(viewsets.ViewSet):
+    pagination_class = FirmasResultSetPagination
+
+    def list(self, request):
+        ultimo_compromiso = CompromisoDePago.objects.order_by('-fecha_carga_comp_pdf').first()
+
+        # Si no hay compromiso, devuelve una lista vacía
+        if not ultimo_compromiso:
+            return Response([], status=status.HTTP_200_OK)
+
+        firmas_ultimo_compromiso = FirmaCompPagoAlumno.objects.filter(compromiso_de_pago=ultimo_compromiso)
+
+        serializer = FirmaCompPagoAlumnoCompletoSerializer(firmas_ultimo_compromiso, many=True)
+        data = serializer.data
+
+        return Response(data)
+
+class AlumnosNoFirmaronUltimoCompromisoView(viewsets.ViewSet):
+    pagination_class = FirmasResultSetPagination
+
+    def list(self, request):
+        ultimo_compromiso = CompromisoDePago.objects.order_by('-fecha_carga_comp_pdf').first()
+
+        # Si no hay compromiso, devuelve una lista vacía
+        if not ultimo_compromiso:
+            return Response([], status=status.HTTP_200_OK)
+        
+        alumnos_firmaron_ids = FirmaCompPagoAlumno.objects.filter(
+            compromiso_de_pago=ultimo_compromiso
+        ).values_list('alumno_id', flat=True)
+
+        alumnos_no_firmaron = Alumno.objects.exclude(user__dni__in=alumnos_firmaron_ids)
+
+        # Si no hay alumnos que no han firmado, también puedes devolver una lista vacía
+        if not alumnos_no_firmaron.exists():
+            return Response([], status=status.HTTP_200_OK)
+
+        serializer = AlumnoSerializer(alumnos_no_firmaron, many=True)
+        data = serializer.data
+
+        return Response(data, status=status.HTTP_200_OK)
+
+
 class FirmarCompromisoView(APIView):
 
     def post(self, request, alumno_id=None):
