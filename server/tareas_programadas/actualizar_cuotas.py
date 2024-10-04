0bbdd05f8c1303c23a0a8719106b152c867a4ec2
ulisespallares888 <ulisespallares8@gmail.com>
2024-10-04 03:07:15 -0300

    Actualizar cuotas mejoras v2

--- server/tareas_programadas/actualizar_cuotas.py
+++ server/tareas_programadas/actualizar_cuotas.py
@@ -18,15 +18,121 @@ DATABASES = {
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 django.setup()
 
-# my imports
-from server.pagos.models import Cuota, CompromisoDePago, LineaDePago , FirmaCompPagoAlumno
+#Verificación
+print(f"{datetime.datetime.now()} - Tareas programadas ejecutándose")
+
+
+# Mis imports
+from server.pagos.models import Cuota, CompromisoDePago, LineaDePago, FirmaCompPagoAlumno
 from server.alumnos.models import Alumno
 from server.materias.models import Materia, MateriaAlumno
 from server.emails_controller.email_config import configurar_mail, enviar_email, Credenciales
 
-print(f"{datetime.datetime.now()} - tareas programadas ejecutándose")
+print(f"{datetime.datetime.now()} - Tareas programadas ejecutándose")
 
 
+def actualizacion_de_cuotas():
+    alumnos_activos = Alumno.objects.filter(estado_academico="Activo")
+
+    ultimo_compromiso = CompromisoDePago.objects.order_by('-fecha_carga_comp_pdf').first()
+    
+    if not ultimo_compromiso:
+        print("No hay compromisos de pago.")
+        return {}
+
+    alumnos_firm_ult_compdepag = FirmaCompPagoAlumno.objects.filter(compromiso_de_pago=ultimo_compromiso, alumno__in=alumnos_activos)
+    hoy = timezone.now().date()
+    anio_actual = hoy.year
+    alumnos_cuota_vencida = {}
+    cant_max_materias = 2
+
+    if hoy.month < 3 or hoy.month > 12:
+        print("Fuera de rango para actualización.")
+        return {}
+
+    cuatrimestre_analizado = 1 if hoy.month <= 7 else 2
+
+    for alumno in alumnos_activos:
+        cant_materias_alumno = MateriaAlumno.objects.filter(alumno=alumno, anio=anio_actual).count()
+        cuotas_pendientes = Cuota.objects.filter(
+            fecha_vencimiento__lte=hoy,
+            alumno=alumno,
+            estado__in=["Impaga", "Vencida", "Pagada parcialmente"],
+            cuatrimestre=cuatrimestre_analizado
+        ).order_by("nro_cuota")
+
+        if cuotas_pendientes.exists():         
+            fechas_vencimiento_monto = {}
+
+            if cant_materias_alumno > cant_max_materias:
+                if hoy >= ultimo_compromiso.fecha_vencimiento_2:
+                    fechas_vencimiento_monto[ultimo_compromiso.fecha_vencimiento_2] = ultimo_compromiso.monto_completo_2venc
+                elif hoy >= ultimo_compromiso.fecha_vencimiento_3:
+                    fechas_vencimiento_monto[ultimo_compromiso.fecha_vencimiento_3] = ultimo_compromiso.monto_completo_3venc
+            else:
+                if hoy >= ultimo_compromiso.fecha_vencimiento_2:
+                    fechas_vencimiento_monto[ultimo_compromiso.fecha_vencimiento_2] = ultimo_compromiso.monto_reducido__2venc
+                elif hoy >= ultimo_compromiso.fecha_vencimiento_3:
+                    fechas_vencimiento_monto[ultimo_compromiso.fecha_vencimiento_3] = ultimo_compromiso.monto_reducido_3venc
+            
+            for cuota in cuotas_pendientes:
+                if cuota.estado in ["Impaga", "Vencida"]:
+                    cuota.estado = "Vencida"
+                    cuota.fecha_vencimiento = list(fechas_vencimiento_monto.keys())[0]
+                    cuota.monto = list(fechas_vencimiento_monto.values())[0]
+                cuota.save()
+
+                if cuota.fecha_vencimiento < hoy:
+                    alumno.estado_financiero = "Inhabilitado" 
+
+              
+                if alumno in alumnos_cuota_vencida:
+                    alumnos_cuota_vencida[alumno].append(cuota)
+                else:
+                    alumnos_cuota_vencida[alumno] = [cuota]
+
+            print("Cuotas actualizadas")
+
+    return alumnos_cuota_vencida
+
+
+def enviar_aviso_de_vencimiento(alumnos_cuota_vencida):
+    if not alumnos_cuota_vencida:
+        print("No hay correos que enviar.")
+        return
+
+    for alumno, cuotas in alumnos_cuota_vencida.items():
+        body = f"Hola {alumno.user.full_name}, "
+        subject = ""
+
+        if alumno.estado_financiero == "Inhabilitado":
+            subject = "Actualización de cuotas y estado financiero"
+            cuotas_vencidas = ", ".join([str(cuota.nro_cuota) for cuota in cuotas])
+            body += f"tienes cuotas vencidas y estás, temporalmente, inhabilitado hasta que abones la/s cuota/s {cuotas_vencidas}.  \nSaludos. \nAtte. Tesorería"
+        else:
+            cuotas_actualizadas = ", ".join([str(cuota.nro_cuota) for cuota in cuotas])
+            subject = "Actualización de cuotas"
+            body += f"se ha actualizado el monto y la fecha de vencimiento de las siguientes cuotas: {cuotas_actualizadas}. \nNo te olvides de abonar. \nSaludos. \nAtte. Tesorería"
+        
+        email_recipient = "tesoreriautnpruebas2024@gmail.com"  
+
+        mensaje = configurar_mail(body, subject, email_recipient)
+        try:
+            enviar_email(mensaje)
+            print(f"Correo enviado a: {email_recipient}")
+        except Exception as e:
+            print(f"Error al enviar correo a {email_recipient}: {e}")
+
+    print("Correos enviados")
+
+
+
+
+
+
+
+
+"""
 def actualizacion_de_cuotas():
     alumnos_activos = Alumno.objects.filter(estado_academico="Activo")
 
@@ -142,4 +248,4 @@ def enviar_aviso_de_vencimiento(alumnos_cuota_vencida):
         
         print("Correos enviados")
     else:
-        print("No hay correos que enviar")
+        print("No hay correos que enviar")"""
