6f533364d6c67a92901931079debd36eff6cb8d5
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2024-10-14 14:42:17 -0300

    Refactor estadisticas app views and serializers

---
+++ server/estadisticas/utils.py
@@ -0,0 +1,39 @@
+from typing import TYPE_CHECKING
+
+from server.alumnos.models import Alumno
+from server.pagos.models import Pago
+
+if TYPE_CHECKING:
+    from django.db.models.manager import BaseManager
+
+
+def get_pagos_por_alumno(mes: int) -> dict:
+    """
+    obtiene los pagos confirmados por alumno y el total del mes
+    Args:
+        mes (int): mes a buscar
+
+    Returns:
+        dict: diccionario con los pagos por alumno y
+        el total del mes y subtotal por alumno
+    """
+    result: dict = {"total_mes": 0, "alumnos": {}}
+    alumnos: BaseManager[Alumno] = Alumno.objects.all()
+
+    for alumno in alumnos:
+        al_dni = alumno.user__dni
+        pagos: BaseManager[Pago] = Pago.objects.filter(
+            alumno=al_dni,
+            fecha__month=mes,
+            estado="Confirmado",
+        )
+        if pagos.exists():
+            al_dni = str(al_dni)
+            result["alumnos"].setdefault(al_dni, {"total": 0, "pagos": []})
+            for pago_alumno in pagos:
+                result["alumnos"][al_dni]["pagos"].append(
+                    {"fecha": pago_alumno.fecha, "monto": pago_alumno.monto_confirmado},
+                )
+                result["alumnos"][al_dni]["total"] += pago_alumno.monto_confirmado
+                result["total_mes"] += pago_alumno.monto_confirmado
+    return result
